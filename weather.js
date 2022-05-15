867af8581aa05ecf6c93b3884c4db6fcd402927a
Osokin-Sanya <osokin98os@gmail.com>
2022-05-15 23:30:30 +0300

    ref

--- weather.js
+++ weather.js
@@ -229,12 +229,17 @@ function dataCurrentWeather(data, currentData) {
 
 function autocomplete(inp, arr) {
   const wrapperCity = document.createElement("div");
-  wrapperCity.className = "autocomplete-wrapper";
+  const KEY_DOWN = 40;
+  const KEY_UP = 38;
+  const ENTER = 13;
 
-  $(".citi").appendChild(wrapperCity);
-
+  let selectedIndex = 0;
   let showСitieslength = 0;
-
+  const selector = "autocomplete-active-item";
+  const getItem = (i) => $$(`.autocomplete-item`)[i];
+  wrapperCity.className = "autocomplete-wrapper";
+  $(".citi").appendChild(wrapperCity);
+  
   inp.addEventListener("input", () => {
     wrapperCity.classList.remove("autocomplete-wrapper-none");
     selectedIndex = 0;
@@ -280,20 +285,9 @@ function autocomplete(inp, arr) {
       }
     }
   });
-
-  const KEY_DOWN = 40;
-  const KEY_UP = 38;
-  const ENTER = 13;
-
-  let selectedIndex = 0;
-  const selector = "autocomplete-active-item";
-
-  //const getItem = i => $(`div[data-autocomplete-id="${i}"]`);
-  const getItem = (i) => $$(`.autocomplete-item`)[i];
+ 
 
   inp.addEventListener("keydown", (event) => {
-    // зачем брать в объект?
-
     if (event.keyCode == KEY_DOWN) {
       if (selectedIndex >= showСitieslength) return;
       const prevItem = getItem(selectedIndex - 1);
@@ -301,9 +295,7 @@ function autocomplete(inp, arr) {
       const findedItem = getItem(selectedIndex);
       findedItem && findedItem.classList.add(selector);
       selectedIndex++;
-      // inp.value = $$(`.autocomplete-item`)[selectedIndex - 1].textContent
-
-      if ($(`.autocomplete-item-hover`)) {
+            if ($(`.autocomplete-item-hover`)) {
         $$(".autocomplete-item")[selectedIndex - 2].classList.remove(
           "autocomplete-item-hover"
         );
@@ -319,9 +311,6 @@ function autocomplete(inp, arr) {
       prevItem && prevItem.classList.remove(selector);
       const findedItem = getItem(selectedIndex - 1);
       findedItem && findedItem.classList.add(selector);
-
-      // inp.value = $$(`.autocomplete-item`)[selectedIndex - 1].textContent
-
       $$(".autocomplete-item")[selectedIndex].classList.remove(
         "autocomplete-item-hover"
       );
@@ -338,6 +327,4 @@ function autocomplete(inp, arr) {
 let countries ="Алупка San-Andreas Denmark Djibouti Dominica Saint-Kitts Nevis Saint-Lucia Saint-Vincent Samoa San-Marino Sao-Tome Principe Saudi-Arabia Schaumburg-Lippe Senegal Serbia Seychelles Sierra Leone-Singapore Dominican-Republic Duchy-of-Parma Slovakia Slovenia Solomon Islands  Somalia South Africa South Sudan Spain Sri Lanka Sudan Suriname Sweden Switzerland Syria Алушта Армянск City-17 Залупенск Бахчисарай Белогорск Каменское Амогус-Сити Абобусград Владеславель Джанкой Евпатория Керчь Красноперекопск Саки Севастополь Симферополь Старый Крым Крым-Наш Судак Феодосия Щёлкино Ялта Бар Бершадь Винница Гайсин Жмеринка Казатин Калиновка Ладыжин Могилёв-Подольский Немиров Погребище Тульчин Хмельник Шаргород Ямполь Берестечко Владимир-Волынский Горохов Камень-Каширский Киверцы Ковель Луцк Любомль Нововолынск Рожище Устилуг Апостолово Верхнеднепровск Вольногорск Днепродзержинск Днепропетровск Жёлтые Воды Кривой Рог Марганец Никополь Новомосковск Орджоникидзе Павлоград Перещепино Першотравенск Подгородное Пятихатки Синельниково Терновка Авдеевка Артёмовск Волноваха Горловка Дзержинск Дебальцево Димитров Доброполье Докучаевск Донецк Дружковка Енакиево Ждановка Зугрэс Кировское Краматорск Красноармейск Красный Лиман Константиновка Мариуполь Макеевка Новогродовка Селидово Славянск Снежное Соледар Торез Угледар Харцызск Шахтёрск Ясиноватая Андрушёвка Барановка Бердичев Житомир Коростень Коростышев Малин Новоград-Волынский Овруч Радомышль Берегово Виноградов Иршава Мукачево Перечин Рахов Свалява Тячев Ужгород Хуст Чоп Бердянск Васильевка Вольнянск Гуляйполе Днепрорудное Запорожье Каменка-Днепровская Мелитополь Молочанск Орехов Пологи Приморск Токмак Энергодар Болехов Бурштын Галич Городенка Долина Ивано-Франковск Калуш Коломыя Косов Надворная Рогатин Снятын Тысменица Тлумач Яремче Белая Церковь Березань Богуслав Борисполь Боярка Бровары Буча Васильков Вишнёвое Вышгород Ирпень Кагарлык Киев Мироновка Обухов Переяслав-Хмельницкий Припять Ржищев Сквира Славутич Тараща Тетиев Узин Украинка Фастов Чернобыль Яготин Александрия Бобринец Гайворон Долинская Знаменка Кировоград Малая Виска Новомиргород Новоукраинка Светловодск Александровск Алмазная Алчевск Антрацит Брянка Вахрушево Горное Зимогорье Золотое Зоринск Краснодон Красный Луч Лисичанск Луганск Лутугино Миусинск Молодогвардейск Новодружеск Новопсков Первомайск Перевальск Петровское Попасная Приволье Ровеньки Рубежное Сватово Свердловск Северодонецк Старобельск Стаханов Суходольск Счастье Теплогорск Червонопартизанск Белз Бобрка Борислав Броды Буск Великие Мосты Глиняны Городок Добромиль Дрогобыч Дубляны Жидачов Жолква Золочев Каменка-Бугская Львов Мостиска Перемышляны Пустомыты Рава-Русская Радехов Рудки Самбор Сколе Сокаль Старый Самбор Стрый Трускавец Угнев Хыров Червоноград Яворов Баштанка Вознесенск Николаев Новая Одесса Новый Буг Очаков Первомайск Снигирёвка Южноукраинск Ананьев Арциз Балта Белгород-Днестровский Болград Измаил Ильичёвск Килия Кодыма Котовск Одесса Татарбунары Теплодар Южное Гадяч Глобино Гребёнка Зеньков Карловка Кременчуг Кобеляки Комсомольск Лохвица Лубны Миргород Пирятин Полтава Хорол Червонозаводское Березне Дубно Дубровица Здолбунов Корец Костополь Кузнецовск Острог Радивилов Ровно Сарны Ахтырка Белополье Бурынь Глухов Кролевец Конотоп Лебедин Путивль Ромны Середина-Буда Сумы Тростянец Шостка Бережаны Борщёв Бучач Залещики Збараж Зборов Кременец Лановцы Монастыриска Подволочиск Подгайцы Почаев Скалат Тернополь Теребовля Чортков Шумск Балаклея Барвенково Богодухов Валки Великий Бурлук Волчанск Дергачи Змиев Изюм Красноград Купянск Лозовая Люботин Мерефа Первомайский Харьков Чугуев Берислав Геническ Голая Пристань Каховка Новая Каховка Скадовск Таврийск Херсон Цюрупинск Волочиск Городок Деражня Дунаевцы Изяслав Каменец-Подольский Красилов Нетешин Полонное Славута Староконстантинов Хмельницкий Шепетовка Ватутино Городище Жашков Звенигородка Золотоноша Каменка Канев Корсунь-Шевченковский Монастырище Смела Тальное Умань Христиновка Черкассы Чигирин Шпола Бахмач Бобровица Борзна Городня Десна Ичня Корюковка Мена Нежин Новгород-Северский Носовка Прилуки Седнев Семёновка Чернигов Щорс Вашковцы Вижница Герца Заставна Кицмань Новоднестровск Новоселица Сокиряны Сторожинец Хотин".split(" ");
 autocomplete($("#myInput"), countries);
 
-// const { charType } = e.target.dataset;
-// const charType = e.target.dataset.charType;
-// const charType = e.target.dataset['charType'];
+ 
